8d7e4b497b103e8b17e8bab29a1873b0b219a882
Lucca <lucca.anderle1512@gmail.com>
2025-03-12 09:17:10 -0300

    Exemplo de Vetor

---
+++ Desenvolvimento_de_Software/Aula4/aula4.java/src/ExemploVetor.java
@@ -0,0 +1,10 @@
+public class ExemploVetor {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50};
+
+        System.out.println("Elementos de vetor: ");
+        for (int i = 0; i < nums.length; i++){
+            System.out.println("Indice "+ i + ": " + nums[i]);
+        }
+    }
+}
